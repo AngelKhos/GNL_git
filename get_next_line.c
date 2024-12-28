43fce414f37c3c2140f0e65e9ae0323cb9a8a5cf
AngelKhos <aurorethomas95@gmail.com>
2024-12-28 14:39:05 +0100

    bebou

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: authomas <authomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:12:14 by authomas          #+#    #+#             */
-/*   Updated: 2024/12/18 15:14:54 by authomas         ###   ########lyon.fr   */
+/*   Updated: 2024/12/28 14:38:26 by authomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 char	*get_next_line(int fd)
 {
+	char		line[BUFFER_SIZE];
+	static char	buff[BUFFER_SIZE];
 
+	read(fd, line, BUFFER_SIZE);
+
+	return (line);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,17 @@
 /*   By: authomas <authomas@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:12:18 by authomas          #+#    #+#             */
-/*   Updated: 2024/12/18 15:13:24 by authomas         ###   ########lyon.fr   */
+/*   Updated: 2024/12/19 16:17:49 by authomas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <unistd.h>
 
 #endif
